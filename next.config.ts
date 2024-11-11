12d0afa8c766e28a8ae6e1e255fce3bb1b584033
Jiaqi He <16203138+JiaqiHe@users.noreply.github.com>
2024-11-10 19:50:25 -0800

    remove basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/jiaqihe.github.io',
+  // basePath: '/jiaqihe.github.io',
 };
 
 export default nextConfig;
