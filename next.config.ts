133f9413ff92b710225378aa0d0c58a8bec13b8c
Jiaqi He <16203138+JiaqiHe@users.noreply.github.com>
2024-11-10 20:40:33 -0800

    clean up main

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // output: 'export',
-  // reactStrictMode: true,
-  basePath: '/jiaqihe.github.io',
-};
-
-export default nextConfig;
