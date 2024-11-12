e9156985eb5b52bcae0ac23e3c3f1af8ce9f4bda
Jiaqi He <16203138+JiaqiHe@users.noreply.github.com>
2024-11-11 21:42:06 -0800

    try again by removing the basepath setting
    per cursor, it does not need to be set

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   output: 'export',  // 启用静态导出
   images: {
     unoptimized: true  // GitHub Pages 不支持 Next.js 的图片优化
-  },
-  basePath: '/jiaqihe.github.io'
+  }
 };
 
 module.exports = nextConfig; 
