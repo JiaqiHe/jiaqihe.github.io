763a0c3482dd095dc878b6f22bcc3004ecc0d627
Jiaqi He <16203138+JiaqiHe@users.noreply.github.com>
2024-11-11 21:23:19 -0800

    Fix missing semi-colon in Clientlayout.tsx

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { NextUIProvider } from '@nextui-org/react'
-import React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function ClientLayout({
   children,
