bdfb82c2b174b4afc3bc39c3d545c1407944b2de
Jiaqi He <16203138+JiaqiHe@users.noreply.github.com>
2024-11-17 16:42:41 -0800

    Update fish-pond-with-spline

---
+++ src/app/craft/fish-pond-with-spline/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/HEWx5ld1uE56GC3D/scene.splinecode" 
+      />
+    </main>
+  );
+}
